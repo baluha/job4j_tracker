ac7f07b296a158f1a471fe22653a72702852bed0
Администратор <xatowar@gmail.com>
2020-12-20 18:14:56 +0300

    Класс Fix, инициализация desc

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,15 @@
+package ru.job4j.oop;
+
+public class Fix {
+
+
+    private Fix(String desc) {
+
+    }
+
+
+    public static void main(String[] args) {
+        Fix fix = new Fix("Чето там");
+
+    }
+    }
