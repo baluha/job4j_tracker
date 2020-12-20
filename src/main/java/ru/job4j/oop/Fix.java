8a69fd5cd7813a0b3bc6b239b811af880438d3eb
Администратор <xatowar@gmail.com>
2020-12-20 18:50:29 +0300

    1.1.1. Конструктор по умолчанию

--- src/main/java/ru/job4j/oop/Fix.java
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -1,15 +1,9 @@
 package ru.job4j.oop;
 
 public class Fix {
-
-
     private Fix(String desc) {
-
     }
-
-
     public static void main(String[] args) {
         Fix fix = new Fix("Чето там");
-
     }
     }
