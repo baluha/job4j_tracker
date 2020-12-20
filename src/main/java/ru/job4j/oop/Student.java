fb9e766e3a6f473f2e8c0f342b5f167ccaf7ce08
Администратор <xatowar@gmail.com>
2020-12-20 18:43:51 +0300

    1.2. Вызов метода объекта

---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Student {
+
+    public void music() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Tra tra tra");
+        }
+    }
+     public void song() {
+         for (int i = 0; i < 3; i++) {
+             System.out.println("I believe i can fly");
+             System.out.println("I believe i can touch the sky");
+         }
+     }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.song();
+    }
+}
