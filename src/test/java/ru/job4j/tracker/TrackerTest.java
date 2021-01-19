70f349467d6e276a428ee7b1fa82c688c48c5a9f
baluha <romanbalushka@gmail.com>
2021-01-19 21:22:06 +0300

    5. Tracker - хранилище

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -27,4 +27,27 @@ public class TrackerTest {
         tracker.replace(id, bugWithDesc);
         assertThat(tracker.findById(id).getName(), is("Bug with description"));
     }
+
+    @Test
+    public void WhenSearchByName() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item();
+        Item item2 = new Item();
+        Item item3 = new Item();
+        Item item4 = new Item();
+        Item item5 = new Item();
+        item1.setName("First");
+        item2.setName("Second");
+        item3.setName("First");
+        item4.setName("Fourth");
+        item5.setName("Third");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        tracker.add(item5);
+        Item [] result = tracker.findByName("First");
+        Item [] expected = new Item [] {item1, item3};
+        assertThat(result, is(expected));
+    }
 }
