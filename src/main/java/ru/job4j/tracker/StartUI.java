74e9e1657572f5eafea9c6106982045e35938da1
baluha <romanbalushka@gmail.com>
2021-01-29 07:44:09 +0300

    2. Scanner и чтение числа из консоли.

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,20 +1,27 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
+
+import java.util.Scanner;
 
 public class StartUI {
 
-    public static void main(String[] args) {
-        Item item = new Item("Vasia",1);
-        System.out.println(item);
-        String datetimeColonPattern = "dd-MM-yyyy HH:mm:ss";
-        DateTimeFormatter timeColonFormatter = DateTimeFormatter.ofPattern(datetimeColonPattern);
-        System.out.println(timeColonFormatter.format(item.getCreated()));
-        Tracker tracker = new Tracker();
-        tracker.add(item);
-        System.out.println(item);
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            this.showMenu();
+            /* if */
+        }
     }
 
+    private void showMenu() {
+        System.out.println("Menu.");
+        /* добавить остальные пункты меню. */
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
+    }
 }
-
-
