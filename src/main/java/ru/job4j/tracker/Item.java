ca38e5ea57d0ad92e64f3b2b8c412b7a9c7b5a52
baluha <romanbalushka@gmail.com>
2021-01-31 04:25:09 +0300

    2. Scanner и чтение числа из консоли.

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -40,6 +40,10 @@ public class Item {
         this.name = name;
     }
 
+    public Item(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "Item{" +
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,13 +9,36 @@ public class StartUI {
         boolean run = true;
         while (run) {
             this.showMenu();
-            /* if */
+            System.out.print("Select: ");
+            int select = Integer.valueOf(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Create a new Item ====");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                           } else if (select == 1) {
+                Item [] item = tracker.findAll();
+                for (int i = 0; i < item.length; i++) {
+                    System.out.println("Id = " + item[i].getId() + ", name = " + item[i].getName());
+                }
+
+            } else if (select == 6) {
+                run = false;
+            }
         }
     }
 
     private void showMenu() {
-        System.out.println("Menu.");
-        /* добавить остальные пункты меню. */
+        System.out.println("Menu.\n" +
+                "0. Add new Item\n" +
+                "1. Show all items\n" +
+                "2. Edit item\n" +
+                "3. Delete item\n" +
+                "4. Find item by Id\n" +
+                "5. Find items by name\n" +
+                "6. Exit Program");
+
     }
 
 
