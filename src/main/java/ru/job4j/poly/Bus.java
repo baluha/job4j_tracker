81027e744127637d1e83f34a4c0972be2ccfbb50
baluha <romanbalushka@gmail.com>
2021-02-01 03:55:41 +0300

    3. Интерфейс Input

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void toGo() {
+
+    }
+
+    @Override
+    public void passengers(int pas) {
+
+    }
+
+    @Override
+    public double price(int liters) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    public void toGo();
+    public void passengers(int pas);
+    public double price(int liters);
+}
