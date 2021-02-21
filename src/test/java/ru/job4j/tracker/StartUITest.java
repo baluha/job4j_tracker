542a07feafd9975acfec8024e5f271527db23a35
baluha <romanbalushka@gmail.com>
2021-02-21 20:23:55 +0300

    0. Что такое исключение.

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -8,6 +8,23 @@ import static org.junit.Assert.*;
 public class StartUITest {
 
     @Test
+    public void whenExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu." + System.lineSeparator() +
+                        "0. Exit" + System.lineSeparator()
+        ));
+    }
+
+    @Test
     public void whenAddItem() {
         String[] answers = {"Fix pc"};
         Input input = new StubInput(answers);
