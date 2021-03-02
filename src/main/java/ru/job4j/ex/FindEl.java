7b3b9af899d57952c35f1e5e3b7b8830c102c33e
baluha <romanbalushka@gmail.com>
2021-03-02 19:34:09 +0300

    0.5. Иерархия исключений и множественный catch.

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,16 +4,17 @@ package ru.job4j.ex;
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (value[i].equals(key)) {
-                rsl = i;
+        int count = 0;
+        for (String i : value) {
+            if (i.equals(key)) {
+                rsl = count;
                 break;
             }
+            count++;
         }
             if (rsl == -1) {
                 throw new ElementNotFoundException("Element not found");
             }
-
         return rsl;
     }
 
---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,43 @@
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User rsl = null;
+        for (User user : users) {
+            if (user.getUsername().equals(login)) {
+                rsl = user;
+            }
+        }
+        if (rsl == null) {
+            throw new UserNotFoundException("User Not Found");
+        }
+        return rsl;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (user.getUsername().length() < 3 || !user.isValid()) {
+            throw new UserInvalidException("Username less then 3 symbols or user is invalid");
+        }
+        return user.isValid();
+    }
+
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        }
+        catch (UserInvalidException e) {
+            e.printStackTrace();
+        }
+        catch (UserNotFoundException nfe) {
+            nfe.printStackTrace();
+        }
+    }
+}
