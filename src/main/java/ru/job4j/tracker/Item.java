ea84c93223d1e151a08b820d6f2ceac147bb7246
baluha <romanbalushka@gmail.com>
2021-04-11 19:37:44 +0300

    0. Коллекции, ArrayList, List, Обобщения

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,8 +2,9 @@ package ru.job4j.tracker;
 
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-public class Item {
+public class Item implements Comparable <Item> {
     private String name;
     private int id;
     private LocalDateTime created = LocalDateTime.now();
@@ -50,5 +51,25 @@ public class Item {
                 "id=" + id+
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id &&
+                Objects.equals(name, item.name) &&
+                Objects.equals(created, item.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id, created);
+    }
+
+    @Override
+    public int compareTo(Item o) {
+        return Integer.compare(id, o.id);
     }
 }
