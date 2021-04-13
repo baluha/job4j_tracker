6be2ade5f9588ee12c51457753030146c58f74d4
baluha <romanbalushka@gmail.com>
2021-04-13 19:50:17 +0300

    2. Преобразования List в Map.

--- src/main/java/ru/job4j/collection/OrderConvert.java
+++ src/main/java/ru/job4j/collection/OrderConvert.java
@@ -1,12 +1,17 @@
 package ru.job4j.collection;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class OrderConvert {
     public static HashMap<String, Order> process(List<Order> orders) {
         HashMap<String, Order> map = new HashMap<>();
+        HashSet<Order> ordersSet = new HashSet<>();
         for (Order order : orders) {
+            ordersSet.add(order);
+        }
+        for (Order order : ordersSet) {
             map.put(order.getNumber(), order);
         }
         return map;
--- src/test/java/ru/job4j/collection/OrderConvertTest.java
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class OrderConvertTest {
     @Test
@@ -16,5 +17,20 @@ public class OrderConvertTest {
         orders.add(new Order("3sfe", "Dress"));
         HashMap<String, Order> map = OrderConvert.process(orders);
         assertThat(map.get("3sfe"), is(new Order("3sfe", "Dress")));
+    }
+    @Test
+    public void whenDuplicate() {
+        List<Order> orders = new ArrayList<>();
+        Order order = new Order("3bce", "Shoes");
+        Order order2 = new Order("3bce", "Shoes");
+        Order order3 = new Order("3sfe", "Dress");
+        orders.add(order);
+        orders.add(order2);
+        orders.add(order3);
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        HashMap<String, Order> expected = new HashMap<>();
+        expected.put(order.getNumber(), order);
+        expected.put(order3.getNumber(), order3);
+        assertThat(map, is(expected));
     }
 }
