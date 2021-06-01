3f9e66ee6c9803a08b52640874250a56583459df
baluha <romanbalushka@gmail.com>
2021-06-01 22:25:31 +0300

    2. Список адресов [#110062]

--- src/main/java/ru/job4j/turistadresslist/Profiles.java
+++ src/main/java/ru/job4j/turistadresslist/Profiles.java
@@ -5,6 +5,9 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
+        return profiles.stream()
+                .map(profile -> profile.getAddress())
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
