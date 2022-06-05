7865fc7c1dcf567639fad6d545757a65b6924149
baluha <romanbalushka@gmail.com>
2022-06-05 17:09:11 +0300

    1. Профилирование.

---
+++ src/main/java/ru/job4j/tracker/ForGCTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.tracker;
+
+import ru.job4j.tracker.action.*;
+import ru.job4j.tracker.input.ConsoleInput;
+import ru.job4j.tracker.input.Input;
+import ru.job4j.tracker.input.ValidateInput;
+import ru.job4j.tracker.output.ConsoleOutput;
+import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.store.MemTracker;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ForGCTest {
+    public void init(Input input, MemTracker tracker, List<UserAction> actions) throws SQLException, InterruptedException {
+        boolean run = true;
+        while (run) {
+            showMenu(actions);
+            int select = input.askInt("Enter select: ");
+            UserAction action = actions.get(select);
+            run = action.execute(input, tracker);
+        }
+    }
+
+    private void showMenu(List<UserAction> actions) {
+        System.out.println("Menu.");
+        for (int i = 0; i < actions.size(); i++) {
+            System.out.printf("%d. %s%n", i, actions.get(i).name());
+        }
+    }
+
+    public static void main(String[] args) {
+        Input input = new ValidateInput(
+                new ConsoleInput()
+        );
+        Output output = new ConsoleOutput();
+        try (SqlTracker tracker = new SqlTracker()) {
+            tracker.init();
+            List<UserAction> actions = List.of(
+                    new GCCreateAction(output),
+                    new ReplaceAction(output),
+                    new DeleteAction(output),
+                    new FindAllAction(output),
+                    new FindByIdAction(output),
+                    new FindByNameAction(output),
+                    new ExitAction()
+            );
+            new StartUIForGC().init(input, tracker, actions);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/ru/job4j/tracker/GCTest.java
+++ src/main/java/ru/job4j/tracker/GCTest.java
@@ -3,12 +3,18 @@ package ru.job4j.tracker;
 import ru.job4j.tracker.model.Item;
 import ru.job4j.tracker.store.MemTracker;
 
+import java.util.List;
+
 public class GCTest {
 
     public static void main(String[] args) {
         MemTracker memTracker = new MemTracker();
-        for (int i = 0; i < 40_000; i++) {
+        for (int i = 0; i < 100_0000; i++) {
             memTracker.add(new Item("item " + i));
+        }
+        List<Item> items = memTracker.findAll();
+        for (int i = 0; i < items.size(); i++) {
+            memTracker.delete(items.get(i).getId());
         }
     }
 }
